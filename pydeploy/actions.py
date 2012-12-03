798bc1dd94e0784c1d44b219e75e67e7533b3b4a
Daniel Hall <daniel@danielhall.me>
2012-12-03 13:48:14 +1100

    Added ansible deployment method

--- pydeploy/actions.py
+++ pydeploy/actions.py
@@ -1,8 +1,24 @@
 import subprocess
+import git, shutil
 
 def argprint(summary, args):
   return summary
 
 def command(summary, args):
   return subprocess.call(args)
+
+def ansible(summary, args):
+  repopath = "/tmp/ansiblerepo"
+  if not os.path.exists(repopath):
+      git.Git().clone(args['ansiblerepo'], repopath)
+
+  repo = git.Repo(repopath)
+  repo.git.reset('HEAD', hard=True)
+  repo.git.pull(rebase=True)
+
+  result = subprocess.call('/usr/bin/ansible-playbook',
+                           repopath + "/" + args['filename'])
+  
+  shutil.rmtree(repopath)
+  return result
 
